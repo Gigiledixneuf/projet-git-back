17e3ccab2ed74a546d226038d035890e5b1b5e51
Impulse <ephraimimpulse17@gmail.com>
2025-04-09 14:54:53 +0100

    ajout du middleware RejectUrlParameters pour englober toutes les routes

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Middleware\RejectUrlParameters;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\{
     ArticleController,
@@ -13,38 +14,48 @@ use App\Http\Controllers\{
 };
 use Illuminate\Http\Request;
 
-// Route pour récupérer l'utilisateur connecté
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
-});
-
-// ------------------- ROUTES PUBLIQUES ------------------- //
-
-// Page d’accueil ou liste des articles
-Route::get('/articles', [ArticleController::class, 'index']);
 
 
-// Affichage d’un article individuel
-Route::get('/articles/{article}', [ArticleController::class, 'show']);
-Route::get('/comments/{article}', [CommentController::class, 'show']);
 
-// Login and Register
-Route::post('/login', [AuthController::class, 'login']);
-Route::post('/register', [AuthController::class, 'register']);
 
-// Categories, Newsletter, tags, vues : accessibles publiquement
-Route::apiResource('/newsletters', NewsletterController::class);
-Route::apiResource('/tags', TagsController::class);
-Route::apiResource('/vues', VueController::class);
-Route::apiResource('/categories', CategoryController::class);
 
-// ------------------- ROUTES PROTÉGÉES ------------------- //
-Route::middleware('auth:sanctum')->group(function () {
-    Route::post('/logout', [AuthController::class, 'logout']);
-    // Like d'un article (protégé)
-    Route::post('/articles/{articleId}/likes', ArticleLikeController::class);
+Route::middleware([RejectUrlParameters::class])->group(function () {
+    // Route pour récupérer l'utilisateur connecté
+    Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+        return $request->user();
+    });
 
-    // Les autres routes (store, update, destroy, etc.) sont protégées
-    Route::apiResource('/articles', ArticleController::class)->except(['index', 'show']);
-    Route::apiResource('/comments', CommentController::class)->except(['show']);
+    // ------------------- ROUTES PUBLIQUES ------------------- //
+
+    // Page d’accueil ou liste des articles
+    Route::get('/articles', [ArticleController::class, 'index']);
+
+
+    // Affichage d’un article individuel
+    Route::get('/articles/{article}', [ArticleController::class, 'show']);
+    Route::get('/comments/{article}', [CommentController::class, 'show']);
+
+    // Login and Register
+    Route::post('/login', [AuthController::class, 'login']);
+    Route::post('/register', [AuthController::class, 'register']);
+
+    // Categories, Newsletter, tags, vues : accessibles publiquement
+    Route::apiResource('/newsletters', NewsletterController::class);
+    Route::apiResource('/tags', TagsController::class);
+    Route::apiResource('/vues', VueController::class);
+    Route::apiResource('/categories', CategoryController::class);
+
+    // ------------------- ROUTES PROTÉGÉES ------------------- //
+    Route::middleware('auth:sanctum')->group(function () {
+        Route::post('/logout', [AuthController::class, 'logout']);
+        // Like d'un article (protégé)
+        Route::post('/articles/{articleId}/likes', ArticleLikeController::class);
+
+        // Les autres routes (store, update, destroy, etc.) sont protégées
+        Route::apiResource('/articles', ArticleController::class)->except(['index', 'show']);
+        Route::apiResource('/comments', CommentController::class)->except(['show']);
+    });
+
+
+
 });
